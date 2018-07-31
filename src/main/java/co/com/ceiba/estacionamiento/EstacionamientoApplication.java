7ff1dc25f818ab727c1126e1ff5ea305930f92b7
Cristian David Castaño Giraldo - Ceiba Software <cristian.castano@ceiba.com.co>
2018-07-31 09:53:41 -0500

    Se suben cambios de prueba

--- src/main/java/co/com/ceiba/estacionamiento/EstacionamientoApplication.java
+++ src/main/java/co/com/ceiba/estacionamiento/EstacionamientoApplication.java
@@ -8,5 +8,9 @@ public class EstacionamientoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstacionamientoApplication.class, args);
+		String a = null;
+		if (a == ""){
+			
+		}
 	}
 }
