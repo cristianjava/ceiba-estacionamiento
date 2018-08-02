54c7201f4ade564fc4e1abaf411f89a88cbf0fee
Cristian David Castaño Giraldo - Ceiba Software <cristian.castano@ceiba.com.co>
2018-08-02 15:08:09 -0500

    Se suben cambios donde se configuran los cocos de los metodos que llevan la logica de negocio y se configuran los test para que apunten a dichos metodos que todavia no tienen el desarrollo de la logica

--- src/test/java/co/com/ceiba/estacionamiento/unitaria/VigilanteTest.java
+++ src/test/java/co/com/ceiba/estacionamiento/unitaria/VigilanteTest.java
@@ -6,27 +6,27 @@ import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 public class VigilanteTest {
-
-	@Test
-	public void esParqueadoTest() {
-		
-		// arrange
-		
-		// act 
-		
-		//assert
-		assertFalse(true);
-	}
-	
-	@Test
-	public void NoParqueadoTest() {
-
-		// arrange
-		
-		// act 
-		
-		//assert
-		assertFalse(true);
-	}
+//
+//	@Test
+//	public void esParqueadoTest() {
+//		
+//		// arrange
+//		
+//		// act 
+//		
+//		//assert
+//		assertFalse(true);
+//	}
+//	
+//	@Test
+//	public void NoParqueadoTest() {
+//
+//		// arrange
+//		
+//		// act 
+//		
+//		//assert
+//		assertFalse(true);
+//	}
 
 }
