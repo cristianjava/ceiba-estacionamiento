d5c7f51b77c514761ecb4bb5615439e62ee97855
Cristian David Castaño Giraldo - Ceiba Software <cristian.castano@ceiba.com.co>
2018-08-02 15:42:37 -0500

    Se suben cambios donde se configuran los cocos de los metodos que llevan la logica de negocio y se configuran los test para que apunten a dichos metodos que todavia no tienen el desarrollo de la logica

--- src/main/java/co/com/ceiba/estacionamiento/negocio/model/Tiquete.java
+++ src/main/java/co/com/ceiba/estacionamiento/negocio/model/Tiquete.java
@@ -7,10 +7,10 @@ public class Tiquete implements Serializable {
 
 	private static final long serialVersionUID = 7052980058096870226L;
 
-	private String placa;
-	private Date fechaIngreso;
-	private Date fechaSalida;
-	private double valorPago;
+	private String placaPojo;
+	private Date fechaIngresoPojo;
+	private Date fechaSalidaPojo;
+	private double valorPagoPojo;
 	private int diasEstadia;
 	private int horasEstadia;
 
@@ -20,44 +20,44 @@ public class Tiquete implements Serializable {
 
 	public Tiquete(String placa, Date fechaIngreso, Date fechaSalida, double valorPago, int diasEstadia, int horasEstadia) {
 		super();
-		this.placa = placa;
-		this.fechaIngreso = fechaIngreso;
-		this.fechaSalida = fechaSalida;
-		this.valorPago = valorPago;
+		this.placaPojo = placa;
+		this.fechaIngresoPojo = fechaIngreso;
+		this.fechaSalidaPojo = fechaSalida;
+		this.valorPagoPojo = valorPago;
 		this.diasEstadia = diasEstadia;
 		this.horasEstadia = horasEstadia;
 	}
 
 	public String getPlaca() {
-		return placa;
+		return placaPojo;
 	}
 
 	public void setPlaca(String placa) {
-		this.placa = placa;
+		this.placaPojo = placa;
 	}
 
 	public Date getFechaIngreso() {
-		return fechaIngreso;
+		return fechaIngresoPojo;
 	}
 
 	public void setFechaIngreso(Date fechaIngreso) {
-		this.fechaIngreso = fechaIngreso;
+		this.fechaIngresoPojo = fechaIngreso;
 	}
 
 	public Date getFechaSalida() {
-		return fechaSalida;
+		return fechaSalidaPojo;
 	}
 
 	public void setFechaSalida(Date fechaSalida) {
-		this.fechaSalida = fechaSalida;
+		this.fechaSalidaPojo = fechaSalida;
 	}
 
 	public double getValorPago() {
-		return valorPago;
+		return valorPagoPojo;
 	}
 
 	public void setValorPago(double valorPago) {
-		this.valorPago = valorPago;
+		this.valorPagoPojo = valorPago;
 	}
 
 	public int getDiasEstadia() {
