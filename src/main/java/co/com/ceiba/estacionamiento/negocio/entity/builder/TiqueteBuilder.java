3a2e3663336e5d4e7822abbeb3b6002d802610e4
Cristian David Castaño Giraldo - Ceiba Software <cristian.castano@ceiba.com.co>
2018-08-02 16:29:52 -0500

    Se suben cambios donde se configuran los cocos de los metodos que llevan la logica de negocio y se configuran los test para que apunten a dichos metodos que todavia no tienen el desarrollo de la logica

--- src/main/java/co/com/ceiba/estacionamiento/negocio/entity/builder/TiqueteBuilder.java
+++ src/main/java/co/com/ceiba/estacionamiento/negocio/entity/builder/TiqueteBuilder.java
@@ -9,24 +9,24 @@ public final class TiqueteBuilder {
 
 	}
 
-	public static Tiquete convertirAObject(TiqueteEntity tiqueteEntity) {
-		Tiquete tiquete = null;
-		if (tiqueteEntity != null) {
-			tiquete = new Tiquete(tiqueteEntity.getPlaca(), tiqueteEntity.getFechaIngreso(), tiqueteEntity.getFechaSalida(),
-					tiqueteEntity.getValorPago(), tiqueteEntity.getDiasParqueo(), tiqueteEntity.getHorasParqueo());
-		}
-		return tiquete;
-	}
-
-	public static TiqueteEntity convertirAEntity(Tiquete tiquete) {
-		TiqueteEntity tiqueteEntity = new TiqueteEntity();
-		tiqueteEntity.setPlaca(tiquete.getPlaca());
-		tiqueteEntity.setFechaIngreso(tiquete.getFechaIngreso());
-		tiqueteEntity.setFechaSalida(tiquete.getFechaSalida());
-		tiqueteEntity.setValorPago(tiquete.getValorPago());
-		tiqueteEntity.setDiasParqueo(tiquete.getDiasEstadia());
-		tiqueteEntity.setHorasParqueo(tiquete.getHorasEstadia());
-		return tiqueteEntity;
-	}
+//	public static Tiquete convertirAObject(TiqueteEntity tiqueteEntity) {
+//		Tiquete tiquete = null;
+//		if (tiqueteEntity != null) {
+//			tiquete = new Tiquete(tiqueteEntity.getPlaca(), tiqueteEntity.getFechaIngreso(), tiqueteEntity.getFechaSalida(),
+//					tiqueteEntity.getValorPago(), tiqueteEntity.getDiasParqueo(), tiqueteEntity.getHorasParqueo());
+//		}
+//		return tiquete;
+//	}
+//
+//	public static TiqueteEntity convertirAEntity(Tiquete tiquete) {
+//		TiqueteEntity tiqueteEntity = new TiqueteEntity();
+//		tiqueteEntity.setPlaca(tiquete.getPlaca());
+//		tiqueteEntity.setFechaIngreso(tiquete.getFechaIngreso());
+//		tiqueteEntity.setFechaSalida(tiquete.getFechaSalida());
+//		tiqueteEntity.setValorPago(tiquete.getValorPago());
+//		tiqueteEntity.setDiasParqueo(tiquete.getDiasEstadia());
+//		tiqueteEntity.setHorasParqueo(tiquete.getHorasEstadia());
+//		return tiqueteEntity;
+//	}
 
 }
