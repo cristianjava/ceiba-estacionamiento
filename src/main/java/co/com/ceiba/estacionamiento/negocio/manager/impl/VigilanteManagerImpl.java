6c33e777a8d91a4d09643ed3f02bc749f1c816c2
Cristian David Castaño Giraldo - Ceiba Software <cristian.castano@ceiba.com.co>
2018-08-08 09:59:36 -0500

    Se suben cambios donde se implementan las pruebas de integración

--- src/main/java/co/com/ceiba/estacionamiento/negocio/manager/impl/VigilanteManagerImpl.java
+++ src/main/java/co/com/ceiba/estacionamiento/negocio/manager/impl/VigilanteManagerImpl.java
@@ -62,6 +62,8 @@ public class VigilanteManagerImpl implements VigilanteManager {
 		} catch (EstacionamientoException e) {
 			LOGGER.info(e.getMessage());
 			throw new EstacionamientoException(e.getMessage());
+		} catch (Exception e) {
+			LOGGER.info(e.getMessage());
 		}
 	}
 
